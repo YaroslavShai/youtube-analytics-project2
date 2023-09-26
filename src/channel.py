2a99f91c7f28866624ed381d5f38f37508994e67
Yaroslav Shai <yaroslav.shay@bk.ru>
2023-09-26 20:11:14 +0300

    add api_key and youtube to Channel

--- src/channel.py
+++ src/channel.py
@@ -11,6 +11,10 @@ print(api_key)
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key = os.getenv("API_KEY")
+
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
